d7c66f903cfbb55437f90218cb76a143b22ca1dc
Ruben Conde <rubconqui@gmail.com>
2025-04-02 19:59:31 +0200

    ejerciocio6

--- Practica2/practica2.cpp
+++ Practica2/practica2.cpp
@@ -146,11 +146,49 @@ bool compensado(const Arbin<T>& a){
 
 /*****************************************************************************/
 //Ejercicio 5
+template <typename T>
+void palabras(Arbin<T>& a, typename Arbin<T>::Iterador r, string p){
+    if(!r.arbolVacio()){
+        p = p + r.observar();
+        palabras(a, a.subIzq(r), p);
+        palabras(a, a.subDer(r), p);
 
+        if(a.subDer(r).arbolVacio() && a.subIzq(r).arbolVacio()){
+            p = p + "\n";
+            cout<<p;
+        }
+    }
+}
+
+template <typename T>
+void palabras(Arbin<T>& a){
+string p;
+palabras(a, a.getRaiz(), p);
+}
 
 /******************************************************************************/
 //Ejercicio 6
 
+int siguienteMayor(ABB<int>& a, typename Arbin<int>::Iterador r, int& x){
+    if(a.esVacio() || a.maximo() <= x){
+        throw NoHaySiguienteMayor();
+    }
+
+
+    if(!r.arbolVacio()){
+        siguienteMayor(a, a.subIzq(r), x);
+        if(x < r.observar()){
+            return r.observar();
+        }
+        siguienteMayor(a, a.subDer(r), x);
+
+    }
+
+}
+
+int siguienteMayor(ABB<int>& a, int x){
+    return siguienteMayor(a, a.getRaiz(), x);
+}
 
 
 /******************************************************************************/
@@ -226,7 +264,7 @@ int main(int argc, char *argv[])
     cout << (compensado(A) ? " SI" : " NO") << endl;
     cout << "Esta B compensado?:";
     cout << (compensado(B) ? " SI" : " NO") << endl << endl;
-    /*
+
     // PALABRAS DE UN ARBOL //
     cout << "PALABRAS DE A:\n";
     palabras(E);
@@ -248,7 +286,7 @@ int main(int argc, char *argv[])
     {
         cout << e.Mensaje() << endl << endl;
     }
-
+    /*
     // POSICION INORDEN //
     BB7.insertar(5); BB7.insertar(1); BB7.insertar(3); BB7.insertar(8); BB7.insertar(6);
     cout << "Posicion Inorden en BB7 de 3: ";
